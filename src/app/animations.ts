555ee48ca6c08c83ed97ea051b46cc54b9a700c3
FahmiDeveloper <coursangularfahmi@gmail.com>
2020-12-06 17:55:25 +0100

    using keyframe

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 export let fade =  trigger('fade', [
 
@@ -10,6 +10,8 @@ export let fade =  trigger('fade', [
 
 ])
 
+
+//Easings
 export let slide =  trigger('slide', [
 
     transition(':enter', [
@@ -17,8 +19,24 @@ export let slide =  trigger('slide', [
       animate(500)
     ]),
 
+    // transition(':leave', [
+    //   animate('0.5s cubic-bezier(.61, .29, .07, 1.02)', style({transform: 'translateX(-100%)'}))
+    // ])
+
+    //using keyframe
     transition(':leave', [
-      animate('0.5s cubic-bezier(.61, .29, .07, 1.02)', style({transform: 'translateX(-100%)'}))
-    ])
+        animate('0.5s ease-out', keyframes([
+            style({
+                offset:.2,
+                opacity:1,
+                transform: 'translateX(20px)'
+            }),
+            style({
+                offset:1,
+                opacity:0,
+                transform: 'translateX(-100%)'
+            })
+        ]))
+      ])
 
 ]);
